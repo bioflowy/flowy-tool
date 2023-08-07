25e1fa0a2293d065de2906a25213d8be1136e9fb
Uehara.Keizo <keizou.bos@gmail.com>
2023-08-07 16:07:32 +0900

    WIP

--- src/utils/expression.test.ts
+++ src/utils/expression.test.ts
@@ -1,10 +1,17 @@
-import { beforeEach, describe, expect, it, vitest } from "vitest";
-import { scanner } from "./expression";
+import { beforeEach, describe, expect, it, vitest } from 'vitest';
+import { do_eval } from './expression';
 describe('scan', () => {
-    
-  it("scan expression",async ()=>{
-    const w = scanner("$('Test')")
-    w.
-
-  })
-})
+    it('scan expression', async () => {
+        const result = do_eval(
+            '$(inputs.test)$("string")',
+            { test: 123 },
+            [],
+            '/tmp/outdir',
+            '/tmp/tmpdir',
+            {},
+            {},
+            true
+        );
+        expect(result).toEr;
+    });
+});
--- src/utils/expression.ts
+++ src/utils/expression.ts
@@ -1,4 +1,7 @@
-type StackState = "default" | "dollar" | "paren" | "brace" | "single_quote" | "double_quote" | "backslash";
+import ivm from 'isolated-vm';
+import { CWLObjectType, CWLOutputType } from '../types';
+
+type StackState = 'default' | 'dollar' | 'paren' | 'brace' | 'single_quote' | 'double_quote' | 'backslash';
 
 class SubstitutionError extends Error {
     constructor(message: string) {
@@ -8,13 +11,13 @@ class SubstitutionError extends Error {
 }
 
 export function scanner(scan: string): [number, number] | null {
-    const DEFAULT: StackState = "default";
-    const DOLLAR: StackState = "dollar";
-    const PAREN: StackState = "paren";
-    const BRACE: StackState = "brace";
-    const SINGLE_QUOTE: StackState = "single_quote";
-    const DOUBLE_QUOTE: StackState = "double_quote";
-    const BACKSLASH: StackState = "backslash";
+    const DEFAULT: StackState = 'default';
+    const DOLLAR: StackState = 'dollar';
+    const PAREN: StackState = 'paren';
+    const BRACE: StackState = 'brace';
+    const SINGLE_QUOTE: StackState = 'single_quote';
+    const DOUBLE_QUOTE: StackState = 'double_quote';
+    const BACKSLASH: StackState = 'backslash';
 
     let i = 0;
     let stack: StackState[] = [DEFAULT];
@@ -24,11 +27,11 @@ export function scanner(scan: string): [number, number] | null {
         const state = stack[stack.length - 1];
         const c = scan[i];
 
-        switch(state) {
+        switch (state) {
             case DEFAULT:
-                if (c === "$") {
+                if (c === '$') {
                     stack.push(DOLLAR);
-                } else if (c === "\\") {
+                } else if (c === '\\') {
                     stack.push(BACKSLASH);
                 }
                 break;
@@ -39,10 +42,10 @@ export function scanner(scan: string): [number, number] | null {
                 }
                 break;
             case DOLLAR:
-                if (c === "(") {
+                if (c === '(') {
                     start = i - 1;
                     stack.push(PAREN);
-                } else if (c === "{") {
+                } else if (c === '{') {
                     start = i - 1;
                     stack.push(BRACE);
                 } else {
@@ -51,9 +54,9 @@ export function scanner(scan: string): [number, number] | null {
                 }
                 break;
             case PAREN:
-                if (c === "(") {
+                if (c === '(') {
                     stack.push(PAREN);
-                } else if (c === ")") {
+                } else if (c === ')') {
                     stack.pop();
                     if (stack[stack.length - 1] === DOLLAR) {
                         return [start, i + 1];
@@ -65,9 +68,9 @@ export function scanner(scan: string): [number, number] | null {
                 }
                 break;
             case BRACE:
-                if (c === "{") {
+                if (c === '{') {
                     stack.push(BRACE);
-                } else if (c === "}") {
+                } else if (c === '}') {
                     stack.pop();
                     if (stack[stack.length - 1] === DOLLAR) {
                         return [start, i + 1];
@@ -82,14 +85,14 @@ export function scanner(scan: string): [number, number] | null {
             case SINGLE_QUOTE:
                 if (c === "'") {
                     stack.pop();
-                } else if (c === "\\") {
+                } else if (c === '\\') {
                     stack.push(BACKSLASH);
                 }
                 break;
             case DOUBLE_QUOTE:
                 if (c === '"') {
                     stack.pop();
-                } else if (c === "\\") {
+                } else if (c === '\\') {
                     stack.push(BACKSLASH);
                 }
                 break;
@@ -98,22 +101,58 @@ export function scanner(scan: string): [number, number] | null {
     }
 
     if (stack.length > 1 && !(stack.length === 2 && (stack[1] === BACKSLASH || stack[1] === DOLLAR))) {
-        throw new SubstitutionError(`Substitution error, unfinished block starting at position ${start}: '${scan.slice(start)}' stack was ${stack}`);
+        throw new SubstitutionError(
+            `Substitution error, unfinished block starting at position ${start}: '${scan.slice(
+                start
+            )}' stack was ${stack}`
+        );
     }
 
     return null;
+}
+function needs_parsing(snippet: any): boolean {
+    return typeof snippet === 'string' && (snippet.includes('$(') || snippet.includes('${'));
+}
+
+export function do_eval(
+    ex: CWLOutputType,
+    jobinput: CWLObjectType,
+    requirements: CWLObjectType[],
+    outdir: string,
+    tmpdir: string,
+    resources: { [key: string]: number },
+    context: CWLOutputType,
+    strip_whitespace: boolean
+): CWLOutputType {
+    /** """
+    Evaluate the given CWL expression, in context.
+
+    :param timeout: The maximum number of seconds to wait while executing.
+    """
+    **/
+    const runtime: { [key: string]: any } = { ...resources };
+    runtime['tmpdir'] = tmpdir ?? null;
+    runtime['outdir'] = outdir ?? null;
+
+    const rootvars = { inputs: jobinput, self: context, runtime: runtime };
+
+    if (typeof ex === 'string' && needs_parsing(ex)) {
+        return interpolate(ex, rootvars, '', (strip_whitespace = strip_whitespace));
+    } else {
+        return ex;
+    }
 }
 
 export function interpolate(
     scan: string,
     rootvars: any, // CWLObjectType in python, replace with the appropriate type
-    jslib: string = "",
-    fullJS: boolean = false,
+    jslib: string = '',
     strip_whitespace: boolean = true,
     escaping_behavior: number = 2,
     js_engine: any = null, // JSEngine in python, replace with the appropriate type
     kwargs: any = {}
-): any { // CWLOutputType in python, replace with the appropriate type
+): any {
+    // CWLOutputType in python, replace with the appropriate type
     if (strip_whitespace) {
         scan = scan.trim();
     }
@@ -123,11 +162,8 @@ export function interpolate(
     while (w) {
         parts.push(scan.slice(0, w[0]));
 
-        if (scan[w[0]] === "$") {
-            js_engine = js_engine || get_js_engine();
-            let e = evaluator(
-                js_engine, scan.slice(w[0] + 1, w[1]), rootvars, jslib, fullJS, kwargs
-            );
+        if (scan[w[0]] === '$') {
+            let e = evaluator(scan.slice(w[0] + 1, w[1]), rootvars, jslib);
             if (w[0] === 0 && w[1] === scan.length && parts.length <= 1) {
                 return e;
             }
@@ -137,17 +173,17 @@ export function interpolate(
                 leaf = JSON.parse(leaf);
             }
             parts.push(leaf);
-        } else if (scan[w[0]] === "\\") {
+        } else if (scan[w[0]] === '\\') {
             if (escaping_behavior === 1) {
                 let e = scan[w[1] - 1];
                 parts.push(e);
             } else if (escaping_behavior === 2) {
                 let e = scan.slice(w[0], w[1] + 1);
-                if (e === "\\$(" || e === "\\${") {
+                if (e === '\\$(' || e === '\\${') {
                     parts.push(e.slice(1));
                     w = [w[0], w[1] + 1];
-                } else if (e[1] === "\\") {
-                    parts.push("\\");
+                } else if (e[1] === '\\') {
+                    parts.push('\\');
                 } else {
                     parts.push(e.slice(0, 2));
                 }
@@ -159,79 +195,20 @@ export function interpolate(
         w = scanner(scan);
     }
     parts.push(scan);
-    return parts.join("");
+    return parts.join('');
 }
 
-import { JSEngine, CWLObjectType, CWLOutputType, WorkflowException, JavascriptException } from '<適切なパスまたはライブラリ名>'; // 対応するモジュールをインポートする必要があります。
-import { inspect } from 'util';
-import { isAsyncFunction } from '<適切なパスまたはライブラリ名>'; // この関数の実装が必要です。
-import { runUntilComplete } from '<適切なパスまたはライブラリ名>'; // この関数の実装が必要です。
+function evaluator(expr: string, obj: CWLObjectType, jsLib: string, kwargs?: any): CWLOutputType {
+    const isolate = new ivm.Isolate({ memoryLimit: 128 });
 
-async function evaluator(
-    jsEngine: JSEngine,
-    ex: string,
-    obj: CWLObjectType,
-    jsLib: string,
-    fullJS: boolean,
-    kwargs?: any,
-): Promise<CWLOutputType | null> {
-    const match = paramRe.match(ex);
-    let expressionParseException: WorkflowException | null = null;
+    // 新しいContextを作成する
+    const context = isolate.createContextSync();
 
-    if (match !== null) {
-        const firstSymbol = match[1];
-        const firstSymbolEnd = match.index + firstSymbol.length;
+    // Contextに基本的なconsole.log関数を注入する
+    context.global.setSync('runtime', new ivm.ExternalCopy(obj).copyInto());
 
-        if (firstSymbolEnd + 1 === ex.length && firstSymbol === "null") {
-            return null;
-        }
-        try {
-            if (!(firstSymbol in obj)) {
-                throw new WorkflowException(`${firstSymbol} is not defined`);
-            }
-
-            if (isAsyncFunction(jsEngine.regexEval)) {
-                return await runUntilComplete<CWLOutputType>(
-                    jsEngine.regexEval(
-                        firstSymbol,
-                        ex.slice(firstSymbolEnd, -1),
-                        <CWLOutputType>obj[firstSymbol],
-                        kwargs,
-                    ),
-                );
-            } else {
-                return <CWLOutputType>jsEngine.regexEval(
-                    firstSymbol,
-                    ex.slice(firstSymbolEnd, -1),
-                    <CWLOutputType>obj[firstSymbol],
-                    kwargs,
-                );
-            }
-        } catch (werr) {
-            if (werr instanceof WorkflowException) {
-                expressionParseException = werr;
-            }
-        }
-    }
-    if (fullJS) {
-        if (isAsyncFunction(jsEngine.eval)) {
-            return await runUntilComplete<CWLOutputType>(
-                jsEngine.eval(ex, jsLib, kwargs),
-            );
-        } else {
-            return <CWLOutputType>jsEngine.eval(ex, jsLib, kwargs);
-        }
-    } else {
-        if (expressionParseException !== null) {
-            throw new JavascriptException(
-                `Syntax error in parameter reference '${ex.slice(1, -1)}': ${expressionParseException}. This could be ` +
-                "due to using Javascript code without specifying InlineJavascriptRequirement.",
-            );
-        } else {
-            throw new JavascriptException(
-                `Syntax error in parameter reference '${ex}'. This could be due ` +
-                "to using Javascript code without specifying InlineJavascriptRequirement.",
-            );
-        }
-    }
+    // Contextにスクリプトを実行する
+    const script = isolate.compileScriptSync(expr);
+    const rslt = script.runSync(context);
+    return rslt;
 }
